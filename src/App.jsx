f9f2ce39957c58e83e585e3da656e9e10f9c431a
kdeep25 <deepikaseran@yahoo.com>
2026-02-02 19:21:43 +0530

    Removed login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Stats from "./pages/Stats";
 import Profile from "./pages/Profile";
@@ -10,49 +9,18 @@ import Layout from "./components/Layout";
 export default function App() {
   return (
     <HashRouter>
-      <Routes>
+      <Layout>
+        <Routes>
+          {/* Default page */}
+          <Route path="/" element={<Dashboard />} />
 
-        {/* Login page (no sidebar) */}
-        <Route path="/" element={<Login />} />
-
-        {/* Pages WITH sidebar layout */}
-        <Route
-          path="/dashboard"
-          element={
-            <Layout>
-              <Dashboard />
-            </Layout>
-          }
-        />
-
-        <Route
-          path="/stats"
-          element={
-            <Layout>
-              <Stats />
-            </Layout>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <Layout>
-              <Profile />
-            </Layout>
-          }
-        />
-
-        <Route
-          path="/skills"
-          element={
-            <Layout>
-              <Skills />
-            </Layout>
-          }
-        />
-
-      </Routes>
+          {/* Other pages */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </Layout>
     </HashRouter>
   );
 }
